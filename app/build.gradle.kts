61f5d27600fcbfeeec3884c1402fe38dfc3bfb8b
Alexander Nitiola <cre8tor.alexander@gmail.com>
2023-10-26 19:51:59 +0100

    added compose related dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,11 +74,19 @@ dependencies {
     // Json Serializer
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    // Compose Dependencies
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation ("androidx.activity:activity-compose:1.8.0")
+    runtimeOnly("androidx.compose.ui:ui:1.5.4")
+    runtimeOnly("androidx.compose.ui:ui-tooling-preview:1.5.4")
+    implementation("androidx.compose.ui:ui-test-junit4:1.5.4")
+    runtimeOnly("androidx.compose.ui:ui-tooling:1.5.4")
+
     // base plugins
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.test.espresso:espresso-core:3.5.1")
 }
