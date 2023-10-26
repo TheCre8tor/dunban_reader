6e690cff3a4d1c72daf54f54068918f39dd1fdbd
Alexander Nitiola <cre8tor.alexander@gmail.com>
2023-10-26 19:21:25 +0100

    added hilt/dagger dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,12 @@ dependencies {
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
 
+    //Dagger - Hilt
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    implementation("com.google.dagger:hilt-android-compiler:2.48.1")
+    implementation("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
     // base plugins
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
