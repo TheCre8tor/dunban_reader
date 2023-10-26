f5e791dea01583fba85935a030c6548a2fd5a09a
Alexander Nitiola <cre8tor.alexander@gmail.com>
2023-10-26 19:35:07 +0100

    added coil image dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
+    // Coil
+    implementation("io.coil-kt:coil-compose:1.4.0")
+
     // base plugins
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
